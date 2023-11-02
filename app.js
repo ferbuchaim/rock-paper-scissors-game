47f0461bc3af31f90613a6dabe152787a6cb4439
Ferbuchaim <buchaim.fernando@gmail.com>
2023-11-02 11:00:05 -0300

    feat: add selection logic

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
-  console.log('Game is starting...')
+let gameIsRunning = false;
+
+function getPlayerChoice() {
+  let selection = prompt('Rock, paper or scissors?', '').toLowerCase();
+  if (selection !== 'rock' && selection !== 'paper' && selection !== 'scissors') {
+    alert('Invalid choice! We chose rock for you!');
+    selection = 'rock';
+  }
+  return selection;
 }
 
+function startGame() {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log('Game is starting...');
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
+}
 
 startGameBtn.addEventListener('click', startGame);
