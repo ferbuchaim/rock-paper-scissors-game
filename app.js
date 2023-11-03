0b600b830d2c65483ae8c2cd83d49ce1337fa97c
Ferbuchaim <buchaim.fernando@gmail.com>
2023-11-03 20:23:26 -0300

    feat: add winner alert and remove auto select

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ let gameIsRunning = false;
 function getPlayerChoice() {
   let selection = prompt('Rock, paper or scissors?', '').toLowerCase();
   if (selection !== 'rock' && selection !== 'paper' && selection !== 'scissors') {
-    alert('Invalid choice! We chose rock for you!');
-    selection = 'rock';
+    alert('Invalid choice! Pick again!');
+    return;
   }
   return selection;
 }
@@ -43,9 +43,22 @@ function startGame() {
   gameIsRunning = true;
   console.log('Game is starting...');
   const playerSelection = getPlayerChoice();
+  if (!playerSelection) {
+    gameIsRunning = false;
+    return startGame();
+  }
   const pcSelection = getPcChoice();
   const winner = checkWinner(pcSelection, playerSelection);
-  console.log('Winner is: ', winner);
+  let message;
+  if (winner === 'draw') {
+    message = `You picked ${playerSelection}, PC picked ${pcSelection}, therefore you have a draw.`;
+  } else if (winner === 'player') {
+    message = `You picked ${playerSelection}, PC picked ${pcSelection}, therefore you won.`;
+  } else {
+    message = `You picked ${playerSelection}, PC picked ${pcSelection}, therefore you lost.`;
+  }
+  alert(message);
+  gameIsRunning = false;
 }
 
 startGameBtn.addEventListener('click', startGame);
